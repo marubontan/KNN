5ef542b3fdfcd31ec4fe5b57701c3b3a6f8129fa
marubontan <marubontan@gmail.com>
2018-04-01 16:38:35 -0300

    move execution sample to example directory

--- src/knn.jl
+++ src/knn.jl
@@ -60,28 +60,3 @@ function splitTrainTest(data, at = 0.7)
     test_ind = view(ind, (floor(Int, at*n)+1):n)
     return data[train_ind,:], data[test_ind,:]
 end
-
-function main()
-    df = readtable("../data/iris.csv", header=true)
-
-    trainData, testData = splitTrainTest(df)
-
-    xTrain = trainData[:, [:SepalLength, :SepalWidth, :PetalLength, :PetalWidth]]
-    yTrain = trainData[:, [:Species]]
-    xTest = testData[:, [:SepalLength, :SepalWidth, :PetalLength, :PetalWidth]]
-    yTest = testData[:, [:Species]]
-
-    knn = KNN(xTrain, yTrain)
-    predicted = predict(knn, xTest; method="minkowski")
-
-    accurate = 0
-    yTestArray = Array(yTest)
-    for i in 1:length(predicted)
-        if yTestArray[i] == predicted[i]
-            accurate += 1
-        end
-    end
-    println(accurate/length(predicted))
-end
-
-main()
